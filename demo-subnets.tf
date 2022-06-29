3f1433b5875a4d914461541dacaa16133d7bd9b9
Anibal Linares <48930195+anibal85@users.noreply.github.com>
2022-06-29 15:02:32 -0400

    Update demo-subnets.tf

--- demo-subnets.tf
+++ demo-subnets.tf
@@ -17,48 +17,6 @@ output "aws_vpc_id" {
   value = aws_vpc.terraform-vpc.id
 }
 
-## Security Group##
-resource "aws_security_group" "terraform_private_sg" {
-  description = "Allow limited inbound external traffic"
-  vpc_id      = aws_vpc.terraform-vpc.id
-  name        = "terraform_ec2_private_sg"
-
-  ingress {
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 22
-    to_port     = 22
-  }
-
-  ingress {
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 8080
-    to_port     = 8080
-  }
-
-  ingress {
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 443
-    to_port     = 443
-  }
-
-  egress {
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
-    to_port     = 0
-  }
-
-  tags = {
-    Name = "ec2-private-sg"
-  }
-}
-
-output "aws_security_gr_id" {
-  value = aws_security_group.terraform_private_sg.id
-}
 
 ## Create Subnets ##
 resource "aws_subnet" "terraform-subnet_1" {
@@ -73,4 +31,19 @@ resource "aws_subnet" "terraform-subnet_1" {
 
 output "aws_subnet_subnet_1" {
   value = aws_subnet.terraform-subnet_1.id
+}
+
+## Create Subnets 2 ##
+resource "aws_subnet" "terraform-subnet_2" {
+  vpc_id            = aws_vpc.terraform-vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "terraform-subnet_2"
+  }
+}
+
+output "aws_subnet_subnet_2" {
+  value = aws_subnet.terraform-subnet_2.id
 }
